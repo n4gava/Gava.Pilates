2c505903d6920a459c01adf43501699525650d5a
Guilherme Gavazzoni <gg_numbuh4@hotmail.com>
2019-01-06 23:31:34 -0200

    First Migration

--- Gava.Pilates.WebApi/Startup.cs
+++ Gava.Pilates.WebApi/Startup.cs
@@ -29,7 +29,7 @@ namespace Gava.Pilates.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
